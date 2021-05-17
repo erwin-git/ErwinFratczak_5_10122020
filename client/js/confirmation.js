8380f5f1accc947b84794da33eccbf5b8f323e33
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-05-17 14:21:39 +0200

    confirm page titles

--- client/js/confirmation.js
+++ client/js/confirmation.js
@@ -1,5 +1,23 @@
+let confirmation = document.getElementById("confirmation");
+
 //confirm page init
 let confirmDiv = document.createElement("div");
-confirmDiv.setAttribute("id","confirmDiv")
+confirmDiv.setAttribute("id","confirmDiv");
 confirmation.appendChild(confirmDiv);
 
+//OK icon
+let okIcon = document.createElement("i");
+okIcon.className = "far fa-check-circle";
+confirmDiv.appendChild(okIcon);
+
+//Thx h3
+let thxTitle = document.createElement("h3");
+thxTitle.className = "thxTitle"
+thxTitle.innerHTML = "Merci"
+confirmDiv.appendChild(thxTitle);
+
+//Thx h3
+let thxTitle2 = document.createElement("h4");
+thxTitle2.className = "thxTitle2"
+thxTitle2.innerHTML = "Votre commande à bien été enregistrée"
+confirmDiv.appendChild(thxTitle2);
