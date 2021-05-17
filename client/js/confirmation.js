d8f12d6c496a79d15f9d19779cc06509b6a407a2
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-05-17 15:15:40 +0200

    confirmation page js finished

--- client/js/confirmation.js
+++ client/js/confirmation.js
@@ -18,7 +18,7 @@ confirmDiv.appendChild(okIcon);
 //Thx h3
 let thxTitle = document.createElement("p");
 thxTitle.className = "thxTitle"
-thxTitle.innerHTML = "Merci"
+thxTitle.innerHTML = "Merci " + `${contact.firstName}` + " !"
 confirmDiv.appendChild(thxTitle);
 
 //Thx h3
@@ -26,21 +26,77 @@ let thxTitle2 = document.createElement("p");
 thxTitle2.className = "thxTitle2"
 thxTitle2.innerHTML = "Votre commande à bien été enregistrée"
 confirmDiv.appendChild(thxTitle2);
+//-------------------------------------------
+//confirmation number div
+let confNumDiv = document.createElement("div");
+confNumDiv.className = "confNumDiv"
+confirmDiv.appendChild(confNumDiv)
 
-//confirmation number 
+//confirmation number title
+let confNumTitle = document.createElement("p");
+confNumTitle.className = "confNumTitle"
+confNumTitle.innerHTML = "Numéro de la commande :"
+confNumDiv.appendChild(confNumTitle)
+//confirmation number
 let confNum = document.createElement("p");
 confNum.className = "confNum"
-confNum.innerHTML = "Votre commande à bien été enregistrée"
-confirmDiv.appendChild(confNum)
+confNum.innerHTML = `${orderId}`;
+confNumDiv.appendChild(confNum)
 
+//---------------------------------------------------------------------------
+//confirm div2
+let confirmDiv2 = document.createElement("div");
+confirmDiv2.setAttribute("id","confirmDiv2");
+confirmation.appendChild(confirmDiv2);
+
+//------------------------------------------------
 //the delivery adress
-let delivAdress = document.createElement("p");
-delivAdress.className = "delivAdress"
-delivAdress.innerHTML = "Votre commande à bien été enregistrée"
-confirmDiv.appendChild(delivAdress);
+//the delivery adress div
 
-//total price 
+let delivAdressDiv = document.createElement("div");
+delivAdressDiv.setAttribute("id","delivAdressDiv");
+confirmDiv2.appendChild(delivAdressDiv);
+
+//the delivery adress title
+let delivAdressTitle = document.createElement("p");
+delivAdressTitle.className = "delivAdressTitle"
+delivAdressTitle.innerHTML = " Adresse de livraison :"
+delivAdressDiv.appendChild(delivAdressTitle);
+
+//first and last name 
+let flName = document.createElement("p");
+flName.className = "flName"
+flName.innerHTML = `${contact.firstName}` + " " + `${contact.lastName}`
+delivAdressDiv.appendChild(flName);
+//address
+let address = document.createElement("p");
+address.className = "address"
+address.innerHTML = `${contact.address}`;
+delivAdressDiv.appendChild(address);
+//city
+let city = document.createElement("p");
+city.className = "city"
+city.innerHTML = `${contact.city}`
+delivAdressDiv.appendChild(city);
+
+//------------------------------------------------
+//total price
+//total price div
+let totamPriceDiv = document.createElement("div");
+totamPriceDiv.setAttribute("id","delivAdressDiv");
+confirmDiv2.appendChild(totamPriceDiv);
+
+//total price title
+let totamPriceTitle = document.createElement("p");
+totamPriceTitle.className = "totamPriceTitle"
+totamPriceTitle.innerHTML = "Prix total de votre commande :"
+totamPriceDiv.appendChild(totamPriceTitle);
+
 let totamPrice = document.createElement("p");
-totamPrice.className = "thxTitle2"
-totamPrice.innerHTML = "Votre commande à bien été enregistrée"
-confirmDiv.appendChild(totamPrice);
+totamPrice.className = "totamPrice"
+totamPrice.innerHTML = `${total}`/100 + " €"
+totamPriceDiv.appendChild(totamPrice);
+
+localStorage.removeItem('contact');
+localStorage.removeItem('total');
+localStorage.removeItem('orderId');
